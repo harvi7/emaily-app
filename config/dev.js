02f75e3bf050ca37b163c4b20920655dda3364ab
harvi7 <iamharshvirani7@gmail.com>
2019-11-09 22:01:14 -0700

    Users can see survey results

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+    googleClientID: '23832714803-q8ddhvnfr1mufohdm84ta1nho8pmkqdi.apps.googleusercontent.com',
+    googleClientSecret: '3CB6TUTe-Vijbn8NALjmeoDI',
+    mongoURI: 'mongodb+srv://harsh:harshmongo@emaily-ogyml.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'supersecretkey',
+    stripePublishableKey: 'pk_test_mcT3h2cceQN2HDET9EPeiId100FEV70Phj',
+    stripeSecretKey: 'sk_test_CgGzjK9048LXjdXjZq2kBA9N00MiYG9aI5',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'
+};
+
+// password for mongocluster: harshmongo
